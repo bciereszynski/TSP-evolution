62570872f10fcf636647446aca9c9cfeb3daf0e8
bciereszynski <b.ciereszynski@wp.pl>
2024-10-12 19:58:42 +0200

    refactor crossover

--- TSP-evolution/TSP-evolution.cpp
+++ TSP-evolution/TSP-evolution.cpp
@@ -51,29 +51,22 @@ void TSP::scrambleMutation(std::vector<int>& individual, const int k, std::rando
 	}
 }
 
-std::pair<std::vector<int>, std::vector<int>> TSP::pmxCrossover(const std::vector<int>& parent1, const std::vector<int>& parent2, std::random_device& rd) {
-	int n = parent1.size();
-
-	std::vector<int> crossoverPoints = utils::getRandomUniquePositions(n, 2, rd);
-	if (crossoverPoints[0] > crossoverPoints[1]) {
-		std::swap(crossoverPoints[0], crossoverPoints[1]);
-	}
-
-	return { createOffspring(crossoverPoints[0], crossoverPoints[1], parent2, parent1),
-			 createOffspring(crossoverPoints[0], crossoverPoints[1], parent1, parent2) };
+std::pair<std::vector<int>, std::vector<int>> TSP::pmxCrossover(const std::vector<int>& parent1, const std::vector<int>& parent2, int crosspoint1, int crosspoint2, std::random_device& rd) {
+	return { createPmxOffspring(parent2, parent1, crosspoint1, crosspoint2),
+			 createPmxOffspring(parent1, parent2, crosspoint1, crosspoint2) };
 }
 
-std::vector<int> TSP::createOffspring(int k, int l, std::vector<int> parent1, std::vector<int> parent2) {
+std::vector<int> TSP::createPmxOffspring(std::vector<int> parent1, std::vector<int> parent2, int crosspoint1, int crosspoint2) {
 	int n = parent1.size();
 	std::vector<int> offspring(n, -1);
-	for (int i = k; i <= l; ++i) {
+	for (int i = crosspoint1; i <= crosspoint2; ++i) {
 		offspring[i] = parent1[i];
 	}
 
-	for (int i = k; i <= l; ++i) {
-		auto subpartSerarchOffset = n - l - 1;
+	for (int i = crosspoint1; i <= crosspoint2; ++i) {
+		auto subpartSerarchOffset = n - crosspoint2 - 1;
 		// Sprawdź czy dany element rodzica został skopiowany do potomka
-		if (std::find(offspring.begin() + k, offspring.end() - subpartSerarchOffset,
+		if (std::find(offspring.begin() + crosspoint1, offspring.end() - subpartSerarchOffset,
 			parent2[i]) != offspring.end() - subpartSerarchOffset) {
 			continue;
 		}
@@ -81,17 +74,17 @@ std::vector<int> TSP::createOffspring(int k, int l, std::vector<int> parent1, st
 		// El. odpowiadjący offspring2[i] został skopiowany na miejsce el. nieskopiowanego parent2[i]
 		// Sprawdz czy element skopiowany znajduje się w zbiorze krzyżowym rodzica 
 		int correspondingValue;
-		auto correspondingIt = std::find(parent2.begin() + k, parent2.end() - subpartSerarchOffset, offspring[i]);
+		auto correspondingIt = std::find(parent2.begin() + crosspoint1, parent2.end() - subpartSerarchOffset, offspring[i]);
 		if (correspondingIt == parent2.end() - subpartSerarchOffset) {
 			// Jeżeli element nie znajduje się w zbiorze, to znaczy że możemy wstawić nieskopiowany element w jego miejsce
 			correspondingValue = offspring[i];
 		}
 		else {
-			//W przeciwnym wypadku, sprawdzamy jaka wartość została skopiowana w jego miejsce
+			// W przeciwnym wypadku, sprawdzamy jaka wartość została skopiowana w jego miejsce
 			int correspondingIndex = std::distance(parent2.begin(), correspondingIt);
 			correspondingValue = offspring[correspondingIndex];
 		}
-		//Wstawiamy element nieskopiowany w miejsce w którym znajduje się jego element odpowiadający
+		// Wstawiamy element nieskopiowany w miejsce w którym znajduje się jego element odpowiadający
 		auto it = std::find(parent2.begin(), parent2.end(), correspondingValue);
 		int index = std::distance(parent2.begin(), it);
 		offspring[index] = parent2[i];
--- TSP-evolution/TSP.h
+++ TSP-evolution/TSP.h
@@ -8,17 +8,18 @@ class TSP {
 public:
 	std::vector<std::vector<int>> population;
 	TSP(int populationSize, std::vector<Location>locations);
+	std::pair<std::vector<int>, std::vector<int>> pmxCrossover(
+			const std::vector<int>& parent1, const std::vector<int>& parent2,
+			int crosspoint1, int crosspoint2,
+			std::random_device& rd);
 
 private:
 	std::vector<std::vector<int>> generatePopulation(
 		int populationSize,
 		const std::vector<Location>& locations,
 		std::random_device& rd);
-	std::vector<int> createOffspring(
-		int k, int l,
-		std::vector<int> parent1,
-		std::vector<int> parent2);
+	std::vector<int> createPmxOffspring(
+		std::vector<int> parent1, std::vector<int> parent2,
+		int crosspoint1, int crosspoint2);
 	void scrambleMutation(std::vector<int>& individual, const int k, std::random_device& rd);
-	std::pair<std::vector<int>, std::vector<int>>
-		pmxCrossover(const std::vector<int>& parent1, const std::vector<int>& parent2, std::random_device& rd);
 };
