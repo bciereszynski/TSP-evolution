b744a841ebd6a914ec30971098bc7bf319dcc328
bciereszynski <b.ciereszynski@wp.pl>
2024-10-12 18:53:39 +0200

    add pmx crossover

--- TSP-evolution/TSP-evolution.cpp
+++ TSP-evolution/TSP-evolution.cpp
@@ -46,6 +46,62 @@ void scrambleMutation(std::vector<int>& individual, const int k, std::random_dev
 	}
 }
 
+
+std::vector<int> createOffspring(int k, int l, std::vector<int> parent1, std::vector<int> parent2) {
+	int n = parent1.size();
+	std::vector<int> offspring(n, -1);
+	for (int i = k; i <= l; ++i) {
+		offspring[i] = parent1[i];
+	}
+
+	for (int i = k; i <= l; ++i) {
+		auto subpartSerarchOffset = n - l - 1;
+		// Sprawdź czy dany element rodzica został skopiowany do potomka
+		if (std::find(offspring.begin() + k, offspring.end() - subpartSerarchOffset,
+			parent2[i]) != offspring.end() - subpartSerarchOffset) {
+			continue;
+		}
+
+		// El. odpowiadjący offspring2[i] został skopiowany na miejsce el. nieskopiowanego parent2[i]
+		// Sprawdz czy element skopiowany znajduje się w zbiorze krzyżowym rodzica 
+		int correspondingValue;
+		auto correspondingIt = std::find(parent2.begin() + k, parent2.end() - subpartSerarchOffset, offspring[i]);
+		if (correspondingIt == parent2.end() - subpartSerarchOffset) {
+			// Jeżeli element nie znajduje się w zbiorze, to znaczy że możemy wstawić nieskopiowany element w jego miejsce
+			correspondingValue = offspring[i];
+		}
+		else {
+			//W przeciwnym wypadku, sprawdzamy jaka wartość została skopiowana w jego miejsce
+			int correspondingIndex = std::distance(parent2.begin(), correspondingIt);
+			correspondingValue = offspring[correspondingIndex];
+		}
+		//Wstawiamy element nieskopiowany w miejsce w którym znajduje się jego element odpowiadający
+		auto it = std::find(parent2.begin(), parent2.end(), correspondingValue);
+		int index = std::distance(parent2.begin(), it);
+		offspring[index] = parent2[i];
+	}
+
+	for (int i = 0; i < n; ++i) {
+		if (offspring[i] == -1) {
+			offspring[i] = parent2[i];
+		}
+
+	}
+	return offspring;
+};
+
+std::pair<std::vector<int>, std::vector<int>> pmxCrossover(const std::vector<int>& parent1, const std::vector<int>& parent2, std::random_device& rd) {
+    int n = parent1.size();
+
+	std::vector<int> crossoverPoints = utils::getRandomUniquePositions(n, 2, rd);
+	if (crossoverPoints[0] > crossoverPoints[1]) {
+		std::swap(crossoverPoints[0], crossoverPoints[1]);
+	}
+
+	return { createOffspring(crossoverPoints[0], crossoverPoints[1], parent2, parent1),
+			 createOffspring(crossoverPoints[0], crossoverPoints[1], parent1, parent2) };
+}
+
 int main() {
 	const std::string filename = "data.tsp";
 	const int populationSize = 10;
@@ -67,6 +123,6 @@ int main() {
 		generatePopulation(populationSize, locations, rd);
 
 	utils::printPopulation(population);
-
+	
 	return 0;
 }
