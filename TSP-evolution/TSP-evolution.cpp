5ee9956f1cbf6afb86a05cc73babcf0d3d7510f0
bciereszynski <b.ciereszynski@wp.pl>
2024-10-12 16:18:45 +0200

    add initial population generation

--- TSP-evolution/TSP-evolution.cpp
+++ TSP-evolution/TSP-evolution.cpp
@@ -3,18 +3,51 @@
 #include "Location.h"
 #include "utils.h"
 
-int main() {
-	std::string filename = "data.tsp";
+#include <algorithm> 
+#include <random>    
+
+std::vector<std::vector<int>> generatePopulation(
+	int populationSize,
+	const std::vector<Location>& locations,
+	std::random_device& rd) {
+	std::vector<std::vector<int>> population;
+	std::vector<int> locationsIDs;
+
+	for (int i = 0; i < locations.size(); ++i) {
+		locationsIDs.push_back(locations[i].id);
+	}
+
+	std::default_random_engine rng(rd());
+
+	for (int i = 0; i < populationSize; ++i) {
+		std::shuffle(locationsIDs.begin(), locationsIDs.end(), rng);
+		population.push_back(locationsIDs);
+	}
+
+	return population;
 }
+
+int main() {
+	const std::string filename = "data.tsp";
+	const int populationSize = 10;
+	const int k = 3;
 
 	std::vector<Location> locations = utils::loadTSPLIB(filename);
 
-    if (!locations.empty()) {
+	if (!locations.empty()) {
 		utils::printLocationsAndDistances(locations);
-    }
-    else {
-        std::cerr << "ERROR: data not loaded" << std::endl;
-    }
+	}
+	else {
+		std::cerr << "ERROR: data not loaded" << std::endl;
+		return 1;
+	}
 
-    return 0;
+	std::random_device rd;
+
+	std::vector<std::vector<int>> population =
+		generatePopulation(populationSize, locations, rd);
+
+	utils::printPopulation(population);
+
+	return 0;
 }
